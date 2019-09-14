05d9365fe68bb5533710b3a5c8f908743545388f
dherv <damien.herv@gmail.com>
2019-09-14 09:17:23 +0900

    (develop) - Setup production database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,17 @@
-export const config = {
-    host: 'localhost',
-    user: 'root',
-    password: '171026ab',
-    database: 'bjj_notes'
-}
+import 'dotenv/config';
+
+const ENV = process.env.NODE_ENV
+
+export const config = process.env.NODE_ENV === "development" ?
+    {
+        "host": process.env.DB_LOCAL_HOST,
+        "user": process.env.DB_LOCAL_USER,
+        "password": process.env.DB_LOCAL_PASSWORD,
+        "database": process.env.DB_LOCAL_DATABASE,
+    } : {
+        "host": process.env.DB_PROD_HOST,
+        "user": process.env.DB_PROD_USER,
+        "password": process.env.DB_PROD_PASSWORD,
+        "database": process.env.DB_PROD_DATABASE,
+    }
+
